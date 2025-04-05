f9456ef801b6a1a7f66169eb06c529668fd10fb1
francisaguilar21 <francis.aguilar11@gmail.com>
2025-04-05 10:30:42 +0800

    Add PostHogAnalytics using Pliny

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -18,15 +18,15 @@ const siteMetadata = {
   // youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com/in/francisaguilar21',
   locale: 'en-US',
-  // analytics: {
-  //   // If you want to use an analytics provider you have to add it to the
-  //   // content security policy in the `next.config.js` file.
-  //   // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-  //   posthogAnalytics: {
-  //     posthogProjectApiKey: 'phc_GIW8CmUGH4He1qWyN21ln4UR6dRAs683i69OULuzY9s',
-  //     apiHost: 'https://app.posthog.com',
-  //   },
-  // },
+  analytics: {
+    // If you want to use an analytics provider you have to add it to the
+    // content security policy in the `next.config.js` file.
+    // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
+    posthogAnalytics: {
+      posthogProjectApiKey: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+      apiHost: 'https://app.posthog.com',
+    },
+  },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
